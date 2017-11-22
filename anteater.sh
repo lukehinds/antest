ad84516868777b272d7f250079c5f9f75689d350
lhinds <lhinds@redhat.com>
2017-11-22 15:30:28 +0000

    Travis test

--- anteater.sh
+++ anteater.sh
@@ -1,3 +1,3 @@
-curl -H "Authorization: token 6fe140e2681994c474d333113e4c7478352c022c" -X POST \
+curl -H "Authorization token ${GITHUB_TOKEN}" -X POST \
 -d "{\"body\": \"Hello world\"}" \
 "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
